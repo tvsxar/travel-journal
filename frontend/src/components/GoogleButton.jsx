246685b8a7d4f705046bdac773cd0e53b31f348f
Taras Poiatsyka <tvsxar@MacBook-Air---Taras.local>
2025-11-27 18:32:06 +0200

    Add link in GoogleButton for redirection, add api function with base URL

--- frontend/src/components/GoogleButton.jsx
+++ frontend/src/components/GoogleButton.jsx
@@ -6,10 +6,11 @@ function GoogleButton() {
       className="w-full text-sm font-medium shadow-lg border border-gray-100 text-gray-700 
           p-2 rounded-full my-2 mt-4 cursor-pointer hover:bg-gray-100 flex items-center justify-center h-12"
     >
-      <div className="flex items-center justify-center">
+      <a href={`${import.meta.env.VITE_API_URL}/auth/google`}
+      className="flex items-center justify-center">
         <img src={google} className="w-6 h-6 mr-2" />
         Continue with Google
-      </div>
+      </a>
     </button>
   );
 }
