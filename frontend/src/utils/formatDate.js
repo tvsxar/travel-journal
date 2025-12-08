566c1d9002180115c5a0b66df2a3670681dd3715
Taras Poiatsyka <tvsxar@MacBook-Air---Taras.local>
2025-12-08 20:58:37 +0200

    Add formatDate function

---
+++ frontend/src/utils/formatDate.js
@@ -0,0 +1,23 @@
+export default function formatDate(dateString) {
+  const date = new Date(dateString);
+
+  const day = date.getDate();
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const year = date.getFullYear();
+
+  function getDaySuffix(day) {
+    if (day >= 11 && day <= 13) return "th";
+    switch (day % 10) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  }
+
+  return `${day}${getDaySuffix(day)} ${month} ${year}`;
+}
